eeb1556dc6efecc1620f672760f8d6d6e44d1787
Luqman Hakim <lqmnhakim32@gmail.com>
2024-02-26 19:11:54 +0800

    home page commit and added container

---
+++ src/Container/1-home/homepage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './homepage.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <div className="main-headline-body">
+                <div className="perspective-text">
+                    <div className="perspective-line">
+                        <p></p>
+                        <p>hello</p>
+                    </div>
+                    <div className="perspective-line">
+                        <p>hello</p>
+                        <p>I am Luqman</p>
+                    </div>
+                    <div className="perspective-line">
+                        <p>I am Luqman</p>
+                        <p>software engineer</p>
+                    </div>
+                    <div className="perspective-line">
+                        <p>software engineer</p>
+                        <p>ui/ux designer</p>
+                    </div>
+                    <div className="perspective-line">
+                        <p>ui/ux designer</p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HomePage;
